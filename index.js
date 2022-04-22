4df3a9489ef6b53b7191d919eddcef064c2ef44c
shohayabt <shohayabhasan@yahoo.com>
2022-04-23 05:26:44 +0600

    getting started with node-js && express

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+//USE MIDDLEWARE
+app.use(cros());
+app.use(express());
+
 app.get('/', (req , res)=>{
     res.send("HELLO WORLD FROM NODE JS || EXPRESS")
 })
